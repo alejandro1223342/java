7ec0f3135b2b30556b456a6ce9f7ad7cb6b2bc72
alejandro1223342 <alejomuoss@gmail.com>
2024-09-18 17:23:54 -0500

    Se invocan las funciones de Calculadora en TestCalculadora

--- Clases/src/com/krakedev/TestCalculadora.java
+++ Clases/src/com/krakedev/TestCalculadora.java
@@ -17,6 +17,24 @@ public class TestCalculadora {
 		System.out.println("El resultado de la suma es es " + sumaUno);
 
 		System.out.println("El resultado de la resta es " + restaUno);
+		
+		
+		System.out.println("--------------------------");
+		
+		double resultadoMulti;
+		double resultadoDividir;
+		double resultadoPromedio;
+		
+		resultadoMulti=c1.multiplicar(5, 2);
+		resultadoDividir=c1.dividir(8,4);
+		resultadoPromedio=c1.promediar(8, 9, 10);
+		
+		System.out.println("La multiplicacion es "+resultadoMulti);
+		System.out.println("La division es "+resultadoDividir);
+		System.out.println("El promedio es "+resultadoPromedio);
+		
+		
+		c1.mostrarResultado();
 
 	}
 
