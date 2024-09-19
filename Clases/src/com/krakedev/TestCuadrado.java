0ac796ab2ad2e947afab727d5e7db42263c092c9
alejandro1223342 <alejomuoss@gmail.com>
2024-09-19 14:47:14 -0500

    Se implementa el metodo para calcular el perimetro en la calse Rectangulo

---
+++ Clases/src/com/krakedev/TestCuadrado.java
@@ -0,0 +1,41 @@
+package com.krakedev;
+
+public class TestCuadrado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Cuadrado c1 = new Cuadrado();
+		Cuadrado c2 = new Cuadrado();
+		Cuadrado c3 = new Cuadrado();
+		
+		
+		c1.lado=4;
+		c2.lado=6;
+		c3.lado=8;
+		
+		double areaCUno = c1.calcularArea();
+		double areaCDos = c2.calcularArea();
+		double areaCTres = c3.calcularArea();
+		
+		double perimetroCUno = c1.calcularPerimetro();
+		double perimetroCDos = c2.calcularPerimetro();
+		double perimetroCTres = c3.calcularPerimetro();
+		
+		
+		
+		System.out.println("Area Cuadrado Uno :" + areaCUno);
+		System.out.println("Area Cuadrado Dos :" + areaCDos);
+		System.out.println("Area Cuadrado Tres :" + areaCTres);
+		
+		System.out.println("---------------");
+		
+		System.out.println("Perimetro Cuadrado Uno :" + perimetroCUno);
+		System.out.println("Perimetro Cuadrado Dos :" + perimetroCDos);
+		System.out.println("Perimetro Cuadrado Tres :" + perimetroCTres);
+			
+		
+
+	}
+
+}
